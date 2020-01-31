9e312abd8493f7264cc085d3bd877cbd493b815d
AbdelI <homeworkoracle@protonmail.com>
2020-01-31 11:25:57 +0100

    all test passing on logout endpoint, MVP DONE

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -5,16 +5,6 @@ describe("server.js module", () => {
   it("has the right environment for the DB_ENV ", () => {
     expect(process.env.DB_ENV).toBe("testing");
   });
-    
-    
-    
-
-
-//   it("should return a 200 OK", () => {
-//     return request(server)
-//       .get("/api/users")
-//       .expect(200);
-//   });
 
   it("returns the proper content type for the post request ", () => {
     return request(server)
@@ -28,11 +18,16 @@ describe("server.js module", () => {
       .expect("Content-Type", /json/);
   });
 
-//   it("returns the right body for the delete request", () => {
-//     return request(server)
-//       .delete("/api/users/14")
-//       .expect({
-//         removed: 1
-//       });
-//   });
+  it("returns the right body for the delete request", () => {
+    return request(server)
+      .get("/api/auth/logout")
+      .expect({
+        message: "you have successfully logged out"
+      });
+  });
+  it("returns the right content length on delete request ", () => {
+    return request(server)
+      .get("/api/auth/logout")
+      .expect("Content-Length", "46");
+  });
 });
